5787508a059331dc2eeb1cd76cff35074f65f053
isnoth <isnothe@gmail.com>
2018-01-25 19:03:32 +0800

    add appollo graphql tools

--- server/app.js
+++ server/app.js
@@ -5,6 +5,57 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
+const { makeExecutableSchema } = require('graphql-tools');
+
+// Some fake data
+const books = [
+  {
+    title: "Harry Potter and the Sorcerer's stone",
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+// The GraphQL schema in string form
+const typeDefs = `
+  type Query {
+    books: [Book]
+  }
+
+  type Book {
+    title: String,
+    author: String
+  }
+
+`;
+
+// The resolvers
+const resolvers = {
+  Query: { books: () => {
+    return new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        resolve(books)
+      }, 3000)
+    })
+    //return books
+  }},
+};
+
+// Put together a schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
+
+
+const myGraphQLSchema = schema// ... define or import your schema here!
+const PORT = 3000;
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -24,6 +75,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' })); // if you want GraphiQL enabled
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
